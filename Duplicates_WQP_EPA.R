83a811a62d7a204f5f1796ef4ffd1b2131ea43e8
Mike Psaris <MPsaris@WQ100232-BBENNI.deq.state.or.us>
2014-05-21 11:26:32 -0700

    Committing before I alter early code to include unique id's for each row in not.usgs.

--- Duplicates_WQP_EPA.R
+++ Duplicates_WQP_EPA.R
@@ -1,4 +1,3 @@
-#Does this work?
 #There are many duplicate stations. Let's see how many there are and how much overlapping data they have.
 
 #Some sampling sites are labeled "Test Submission". Investigate these.
@@ -15,9 +14,10 @@ mli.real<-paste("NARS_WQX", substr(mli.test, 9, 18), sep="")
 mli.real.len<-sapply(mli.real, function(x){nrow(wqp.data[wqp.data[[1]]==x,])})
 summary(mli.real.len)
 
-#Check for duplicate stations by comparing unique combinatios of Datum, Lat, and Lon. 
+#Check for duplicate stations by comparing unique combinations of Datum, Lat, and Lon. 
 col.nums<-sapply(c("Latitude", "Longitude", "HorizontalCoordinate"), grep, names(not.usgs))
 not.usgs$spatial.id<-paste(not.usgs[[18]], not.usgs[[13]], not.usgs[[12]],sep="_")
+not.usgs$Temp.wqp.epa.id<-paste('temp_wqp_epa_id', seq(1,nrow(not.usgs)), sep="_")
 
 not.usgs.dups<-not.usgs[not.usgs$spatial.id %in% not.usgs[duplicated(not.usgs$spatial.id),'spatial.id'],
                         c('MonitoringLocationIdentifier','spatial.id')]
@@ -28,7 +28,7 @@ nrow(ndups)
 #Create new df with duplicate station data, and remove duplicate rows.
 wqp.dup.data<-wqp.data[wqp.data[['MonitoringLocationIdentifier']] %in% not.usgs.dups[['MonitoringLocationIdentifier']],]
 #Assign unique ids for independent measurements. This field will be used to identify duplicates
-wqp.dup.data$unique.id<-gsub("\\s", "", paste(wqp.dup.data[['ActivityStartDate']], wqp.dup.data[['ActivityDepthHeightMeasureMeasureValue']], 
+wqp.dup.data$unique.meas.id<-gsub("\\s", "", paste(wqp.dup.data[['ActivityStartDate']], wqp.dup.data[['ActivityDepthHeightMeasureMeasureValue']], 
                                               wqp.dup.data[['ProjectIdentifier']], wqp.dup.data[['SampleCollectionMethodMethodIdentifier']], 
                                               wqp.dup.data[['SampleCollectionEquipmentName']], wqp.dup.data[['CharacteristicName']], 
                                               wqp.dup.data[['ResultSampleFractionText']], wqp.dup.data[['ResultMeasureValue']], 
@@ -39,7 +39,10 @@ wqp.dup.data$unique.id<-gsub("\\s", "", paste(wqp.dup.data[['ActivityStartDate']
 wqp.dup.data.lst<-lapply(ndups, function(x){wqp.dup.data[wqp.dup.data[['MonitoringLocationIdentifier']] %in% x[['MonitoringLocationIdentifier']],]})
 lst.nrow<-sapply(wqp.dup.data.lst, nrow)
 summary(lst.nrow)
-lapply(wqp.dup.data.lst, function(x){duplicated(x$unique.id)})
+lst.duplicated<-lapply(wqp.dup.data.lst, function(x){duplicated(x$unique.meas.id)})
 wqp.dup.data.lst[['WGS84_-122.95999999999999_45.484720000000003']]$'MonitoringLocationIdentifier'
-wqp.dup.data.lst[['WGS84_-122.95999999999999_45.484720000000003']]$'unique.id'
+wqp.dup.data.lst[['WGS84_-122.95999999999999_45.484720000000003']]$'unique.meas.id'
+lst.duplicated[['WGS84_-122.95999999999999_45.484720000000003']]
 
+#remove duplicates
+wqp.dup.data$unique.id<-gsub("\\s", "", paste(wqp.dup.data[['MonitoringLocationIdentifier']], wqp.dup.data[['unique.meas.id']], sep="_"))
