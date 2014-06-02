1c366ad9e8507675e0630306c422faab5aea197d
Mike Psaris <MPsaris@WQ100232-BBENNI.deq.state.or.us>
2014-06-02 15:38:46 -0700

    Some USGS station ids did not transfer into the shapefile. Master_List_of_Stations_2.csv fixes this.

--- Assign_LLID.py
+++ Assign_LLID.py
@@ -5,6 +5,7 @@
 # Import necessary modules 
 import arcpy
 import numpy
+import pandas as pd
 import re
 from arcpy import env
 import os.path
@@ -14,7 +15,7 @@ arcpy.env.overwriteOutput = True
 temp_location = "C:/"
 workspace = "C:/assign_llid_temp.gdb"
 temp_gdb = "assign_llid_temp.gdb"
-final_gdb = "Master_List_of_Stations_Results_Tol12000_II.gdb"
+final_gdb = "qaqc_test.gdb"
 original_sampling_stations = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/ShapeFiles/Master_List_of_Stations.shp"
 sampling_stations = "stations_copy"
 stream_network = "F:/Base_Data/DEQ_Data/WQ_2010_IntegratedReport_V3/WQ_2010_IntegratedReport_V3/Assessment.gdb/DEQ_Streams_25APR2013"
@@ -30,6 +31,8 @@ qc_success = "qc_success"
 qc_review = "qc_needs_review"
 outside_threshold = "outside_threshold"
 properties = "RID POINT MEAS"
+qc2_stations = "qc2_stations"
+qc2_streams = "qc2_streams"
 
 # Check to see if a temp geodatabase exists. If not, create it.
 if os.path.exists(temp_location + temp_gdb):
@@ -54,20 +57,22 @@ arcpy.LocateFeaturesAlongRoutes_lr(sampling_stations, stream_network, rid, searc
                                    properties)
 successful_features = arcpy.da.TableToNumPyArray(output_table, 'Unique_ID')['Unique_ID']
 
-#Add QC field to table
-arcpy.AddField_management(output_table, "QAQC", "STRING")
+#Add QC fields to table
+arcpy.AddField_management(output_table, "QAQC1", "STRING")
+arcpy.AddField_management(output_table, "QAQC2", "STRING")
 
-#Now, use character matching to verify that successful rows have matching stream names.
+#Now, begin primary qc by using character matching to verify that successful rows have matching stream names.
 
 stream_names_from_deq_streams = arcpy.da.TableToNumPyArray(stream_network, ['LLID', streams_river_name_field])[['LLID', streams_river_name_field]]
 
-with arcpy.da.UpdateCursor(output_table, [station_river_name_field,'RID','QAQC']) as cursor:
+with arcpy.da.UpdateCursor(output_table, [station_river_name_field,'RID','QAQC1', 'QAQC2']) as cursor:
     for row in cursor:
         deq_streams = stream_names_from_deq_streams[streams_river_name_field][nonzero(stream_names_from_deq_streams['LLID'] == row[1])][0]
         if row[0].replace(" ", "").lower() == deq_streams.replace(" ", "").lower():
-            row[2] = 'Yes'
+            row[2] = 'Reviewed'
+            row[3] = 'Not Required'
         else:
-            row[2] = 'Needs Review'
+            row[2] = 'Needs Secondary Review'
         cursor.updateRow(row)
 
 #Create a 'success' fc and a 'fail' fc
@@ -108,17 +113,46 @@ arcpy.JoinField_management(output_success, 'Unique_ID', output_table, 'Unique_ID
 
 #Now split success fc into one fc with successful qc and one with stations needing review
 arcpy.MakeFeatureLayer_management(output_success, qc_lyr)
-arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC" = 'Yes' """)
+arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC1" = 'Reviewed' """)
 
 if int(arcpy.GetCount_management(qc_lyr).getOutput(0)) == len(successful_features):
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_success))
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_success))
 elif int(arcpy.GetCount_management(qc_lyr).getOutput(0)) == 0:
-    arcpy.CopyFeatures_management(output_success, (temp_location + final_gdb + "/" + qc_review))
+    arcpy.CopyFeatures_management(output_success, (temp_location + temp_gdb + "/" + qc_review))
 elif int(arcpy.GetCount_management(qc_lyr).getOutput(0)) < len(successful_features) and int(arcpy.GetCount_management(qc_lyr).getOutput(0)) > 0:
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_success))
-    arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC" = 'Needs Review' """)
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_review))
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_success))
+    arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC1" = 'Needs Secondary Review' """)
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_review))
 
 arcpy.CopyFeatures_management(output_fail, (temp_location + final_gdb + '/' + outside_threshold))
 arcpy.SelectLayerByAttribute_management(qc_lyr, "CLEAR_SELECTION")
-#arcpy.Delete_management((temp_location + temp_gdb))
+
+#Begin secondary review
+
+#List of Unique_IDs needing review
+#arcpy.AddField_management((temp_location + temp_gdb + "/" + qc_review), "names_tmp", "STRING")
+#with arcpy.da.UpdateCursor((temp_location + temp_gdb + "/" + qc_review), [station_river_name_field,'names_tmp']) as cursor:
+#    for row in cursor:
+#        row[1] = row[0].replace(" ", "").lower()
+#        cursor.updateRow(row)
+
+unique_ids_needing_review = arcpy.da.TableToNumPyArray((temp_location + temp_gdb + "/" + qc_review), ['Unique_ID'])['Unique_ID']
+unique_ids_needing_review2 = unique_ids_needing_review[660:670]
+
+n=[]
+n2=[]
+for uid in unique_ids_needing_review:
+    #uid = unique_ids_needing_review[0]
+    arcpy.MakeFeatureLayer_management((temp_location + temp_gdb + "/" + qc_review), qc2_stations)
+    arcpy.MakeFeatureLayer_management(stream_network, qc2_streams)
+    arcpy.SelectLayerByAttribute_management(qc2_stations, "NEW_SELECTION", """ "UNIQUE_ID" = {} """.format(uid))
+    station_stream_name = arcpy.da.TableToNumPyArray(qc2_stations, [station_river_name_field])[station_river_name_field][0].lower()
+    arcpy.SelectLayerByLocation_management(qc2_streams, 'WITHIN_A_DISTANCE', qc2_stations, search_radius)
+    n.append(int(arcpy.GetCount_management(qc2_streams).getOutput(0)))
+    #print(n)
+    #The REPLACE statement is not included in SQL-92, which is the standard used by file geodatabases, so I use TRIM instead
+    arcpy.SelectLayerByAttribute_management(qc2_streams, "SUBSET_SELECTION", """ TRIM(BOTH ' ' FROM LOWER("{0}")) = '{1}' """.format(streams_river_name_field, station_stream_name))
+    n2.append(int(arcpy.GetCount_management(qc2_streams).getOutput(0)))
+    #print(n2)
+
+n2s = pd.Series(n2)
