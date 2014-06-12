28e0d61fe898ee009e4ee161e4a405365e3e264d
Mike Psaris <MPsaris@WQ100232-BBENNI.deq.state.or.us>
2014-06-11 17:28:49 -0700

    Processed new stations. Began formatting final station list.

--- Assign_LLID_Additional_LASAR_Stations.py
+++ Assign_LLID_Additional_LASAR_Stations.py
@@ -12,18 +12,18 @@ import os.path
 
 arcpy.env.overwriteOutput = True
 
-temp_location = "C:/"
-workspace = "C:/assign_llid_temp.gdb"
+temp_location = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/"
+workspace = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/assign_llid_temp.gdb"
 temp_gdb = "assign_llid_temp.gdb"
-final_gdb = "qaqc_test.gdb"
-original_sampling_stations = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/ShapeFiles/Master_List_of_Stations.shp"
+final_gdb = "Additional_LASAR_Stations.gdb"
+original_sampling_stations = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/ShapeFiles/Additional_LASAR_Stations.shp"
 sampling_stations = "stations_copy"
 stream_network = "F:/Base_Data/DEQ_Data/WQ_2010_IntegratedReport_V3/WQ_2010_IntegratedReport_V3/Assessment.gdb/DEQ_Streams_25APR2013"
-station_river_name_field = "DESCRIPTIO"
+station_river_name_field = "LOCATION_D"
 streams_river_name_field = "NAME"
 rid = "LLID"
 search_radius = 12000
-output_table = "C:/assign_llid_temp.gdb/out1"
+output_table = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/assign_llid_temp.gdb/out1"
 output_success = "out_success"
 output_fail = "out_fail"
 qc_lyr = "qc_lyr"
@@ -33,8 +33,8 @@ outside_threshold = "outside_threshold"
 properties = "RID POINT MEAS"
 qc2_stations = "qc2_stations"
 qc2_streams = "qc2_streams"
-temp = "C:/assign_llid_temp.gdb/individual_runs"
-reruns = "C:/assign_llid_temp.gdb/reruns"
+temp = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/assign_llid_temp.gdb/individual_runs"
+reruns = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/assign_llid_temp.gdb/reruns"
 
 # Check to see if a temp geodatabase exists. If not, create it.
 if os.path.exists(temp_location + temp_gdb):
@@ -109,7 +109,7 @@ for field in fieldList:
         fields_to_drop.append(field.name)
 
 arcpy.DeleteField_management(output_success, fields_to_drop)
-
+    
 #Merge with output table
 arcpy.JoinField_management(output_success, 'Unique_ID', output_table, 'Unique_ID')
 
@@ -118,72 +118,13 @@ arcpy.MakeFeatureLayer_management(output_success, qc_lyr)
 arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC1" = 'Reviewed' """)
 
 if int(arcpy.GetCount_management(qc_lyr).getOutput(0)) == len(successful_features):
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_success))
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_success))
 elif int(arcpy.GetCount_management(qc_lyr).getOutput(0)) == 0:
-    arcpy.CopyFeatures_management(output_success, (temp_location + temp_gdb + "/" + qc_review))
+    arcpy.CopyFeatures_management(output_success, (temp_location + final_gdb + "/" + qc_review))
 elif int(arcpy.GetCount_management(qc_lyr).getOutput(0)) < len(successful_features) and int(arcpy.GetCount_management(qc_lyr).getOutput(0)) > 0:
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_success))
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_success))
     arcpy.SelectLayerByAttribute_management(qc_lyr, "NEW_SELECTION", """ "QAQC1" = 'Needs Secondary Review' """)
-    arcpy.CopyFeatures_management(qc_lyr, (temp_location + temp_gdb + "/" + qc_review))
+    arcpy.CopyFeatures_management(qc_lyr, (temp_location + final_gdb + "/" + qc_review))
 
 arcpy.CopyFeatures_management(output_fail, (temp_location + final_gdb + '/' + outside_threshold))
 arcpy.SelectLayerByAttribute_management(qc_lyr, "CLEAR_SELECTION")
-
-#Begin secondary review
-
-#List of Unique_IDs needing review
-#arcpy.AddField_management((temp_location + temp_gdb + "/" + qc_review), "names_tmp", "STRING")
-#with arcpy.da.UpdateCursor((temp_location + temp_gdb + "/" + qc_review), [station_river_name_field,'names_tmp']) as cursor:
-#    for row in cursor:
-#        row[1] = row[0].replace(" ", "").lower()
-#        cursor.updateRow(row)
-
-unique_ids_needing_review = arcpy.da.TableToNumPyArray((temp_location + temp_gdb + "/" + qc_review), ['Unique_ID'])['Unique_ID']
-unique_ids_needing_review2 = unique_ids_needing_review[660:670]
-
-#n=[]
-n2=[]
-for uid in unique_ids_needing_review:
-    #uid = unique_ids_needing_review[0]
-    arcpy.MakeFeatureLayer_management((temp_location + temp_gdb + "/" + qc_review), qc2_stations)
-    arcpy.MakeFeatureLayer_management(stream_network, qc2_streams)
-    arcpy.SelectLayerByAttribute_management(qc2_stations, "NEW_SELECTION", """ "UNIQUE_ID" = {} """.format(uid))
-    station_stream_name = arcpy.da.TableToNumPyArray(qc2_stations, [station_river_name_field])[station_river_name_field][0].lower()
-    arcpy.SelectLayerByLocation_management(qc2_streams, 'WITHIN_A_DISTANCE', qc2_stations, search_radius)
-    #n.append(int(arcpy.GetCount_management(qc2_streams).getOutput(0)))
-    #print(n)
-    #The REPLACE statement is not included in SQL-92, which is the standard used by file geodatabases, so I use TRIM instead
-    arcpy.SelectLayerByAttribute_management(qc2_streams, "SUBSET_SELECTION", """ TRIM(BOTH ' ' FROM LOWER("{0}")) = '{1}' """.format(streams_river_name_field, station_stream_name))
-    if int(arcpy.GetCount_management(qc2_streams).getOutput(0)) == 1:
-        # Execute LocateFeaturesAlongRoutes tool for selected station and stream
-        arcpy.LocateFeaturesAlongRoutes_lr(qc2_stations, qc2_streams, rid, search_radius, temp, 
-                                           properties)
-        if arcpy.Exists(rerun):
-            arcpy.Merge_management([temp, reruns])
-        else:
-            arcpy.TableToTable_conversion(temp, reruns[:-7], reruns[-6:])
-    n2.append(int(arcpy.GetCount_management(qc2_streams).getOutput(0)))
-    #print(n2)
-
-n2s = pd.Series(n2)
-index = n2s[n2s == 1].index
-#Unique IDs that match and can be rerun through selection
-qced = unique_ids_needing_review[index]
-
-#Create two new copies of the reviewed shapefile
-
-#Remove duplicates from qc_success
-in_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_success"
-expression = 'isDuplicate( !STATION_ID! )'
-codeblock = """uniqueList = []
-def isDuplicate(inValue):
-    if inValue in uniqueList:
-        return 1
-    else:
-        uniqueList.append(inValue)
-        return 0"""
-
-arcpy.AddField_management(in_file, "Duplicate", "SHORT")
-
-arcpy.CalculateField_management(in_file, "Duplicate", expression, "PYTHON_9.3", 
-                                codeblock)
