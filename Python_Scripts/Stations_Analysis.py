9218c61475a139e635e7c8a9d6110be3ce9d76d1
Mike Psaris <MPsaris@WQ100232-BBENNI.deq.state.or.us>
2014-06-13 16:55:54 -0700

    Made some final modifications to the code.

--- Python_Scripts/Stations_Analysis.py
+++ Python_Scripts/Stations_Analysis.py
@@ -16,10 +16,9 @@ arcpy.env.overwriteOutput = True
 workspace = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList"
 arcpy.env.workspace = workspace
 
-#Two functions to simplify code
+#Define two functions to simplify code
 
-#First function replaces old attributes with the manually edited attribute table
-
+#The first function replaces old attributes with the manually edited attribute tables
 def replace_attributes(out_path, in_feat, merge_file):
     merge_table_path = out_path
     merge_table_name = merge_file[:-4]
@@ -36,7 +35,7 @@ def replace_attributes(out_path, in_feat, merge_file):
     arcpy.JoinField_management(in_feat, 'Unique_ID', (merge_table_path + "/" + merge_table_name), 'Unique_ID')
     arcpy.Delete_management((merge_table_path + "/" + merge_table_name))
 
-#Second, create a function that renames fields.
+#This one renames attribute field names.
 def renameField(fc, old_name, new_name):
     fields = arcpy.ListFields(fc)
     dtype = [x.type for x in fields if x.name == old_name]
@@ -60,51 +59,20 @@ arcpy.SelectLayerByAttribute_management(lstations, "NEW_SELECTION", query)
 arcpy.CopyFeatures_management(lstations, out_feature)
 
 
-#Link manually edited results back up with their shapefiles. I have not figured out how to use functions yet,
-#so for the time being I'm just going to rewrite the scripts for each one.
-
-#Test-run of new function for Additional Lasar Stations
-out_file = "Additional_LASAR_Stations_Edits_test.gdb"
-in_fc = "Additional_LASAR_Stations_Edits_test.gdb/All_stations"
-in_table = "Additional_LASAR_Stations_merge.csv"
-replace_attributes(out_file, in_fc, in_table)
+#Link manually edited results back up with their shapefiles using replace_attributes function
 
 #First for Additional Lasar Stations
-in_feature = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Additional_LASAR_Stations_Edits.gdb/All_stations"
-merge_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Additional_LASAR_Stations_merge.csv"
-merge_table_path = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Additional_LASAR_Stations_Edits.gdb"
-merge_table_name = "Additional_LASAR_Stations_merge"
-fieldList = arcpy.ListFields(in_feature)
-fields_to_drop = []
+out_path = "Additional_LASAR_Stations_Edits_test.gdb"
+in_fc = "Additional_LASAR_Stations_Edits_test.gdb/All_stations"
+in_table = "Additional_LASAR_Stations_merge.csv"
+replace_attributes(out_path, in_fc, in_table)
 
-for field in fieldList:
-    if field.name not in ['Unique_ID', 'Shape','OBJECTID']:
-        fields_to_drop.append(field.name)
-
-arcpy.DeleteField_management(in_feature, fields_to_drop)
-
-arcpy.TableToTable_conversion(merge_file, merge_table_path, merge_table_name)
-arcpy.JoinField_management(in_feature, 'Unique_ID', (merge_table_path + "/" + merge_table_name), 'Unique_ID')
-arcpy.Delete_management((merge_table_path + "/" + merge_table_name))
-pd.read_csv(merge_file, header=0)
 
 #Then for Master_List_of_Stations_Results_Tol12000_II_Edits qc_success
-in_feature = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_success_update"
-merge_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/qc_success.csv"
-merge_table_path = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
-merge_table_name = "qc_success"
-fieldList = arcpy.ListFields(in_feature)
-fields_to_drop = []
-
-for field in fieldList:
-    if field.name not in ['Unique_ID', 'Shape','OBJECTID']:
-        fields_to_drop.append(field.name)
-
-arcpy.DeleteField_management(in_feature, fields_to_drop)
-
-arcpy.TableToTable_conversion(merge_file, merge_table_path, merge_table_name)
-arcpy.JoinField_management(in_feature, 'Unique_ID', (merge_table_path + "/" + merge_table_name), 'Unique_ID')
-arcpy.Delete_management((merge_table_path + "/" + merge_table_name))
+out_path = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
+in_fc = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_success_update"
+in_table = "qc_success.csv"
+replace_attributes(out_path, in_fc, in_table)
 
 #Remove duplicates from qc_success
 in_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_success"
@@ -121,43 +89,24 @@ arcpy.AddField_management(in_file, "Duplicate", "SHORT")
 
 arcpy.CalculateField_management(in_file, "Duplicate", expression, "PYTHON_9.3", 
                                 codeblock)
+#Duplicates were removed manually in ArcGIS. This code can be used to automate it
+with arcpy.da.UpdateCursor(in_file, "Duplicate") as cursor:
+    for row in cursor:
+        if row[0] == 1:
+            cursor.deleteRow()
 
 
-#Then for Master_List_of_Stations_Results_Tol12000_II_Edits outside_threshold
-in_feature = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/outside_threshold_update"
-merge_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/outside_threshold.csv"
-merge_table_path = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
-merge_table_name = "outside_threshold"
-fieldList = arcpy.ListFields(in_feature)
-fields_to_drop = []
-
-for field in fieldList:
-    if field.name not in ['Unique_ID', 'Shape','OBJECTID']:
-        fields_to_drop.append(field.name)
-
-arcpy.DeleteField_management(in_feature, fields_to_drop)
-
-arcpy.TableToTable_conversion(merge_file, merge_table_path, merge_table_name)
-arcpy.JoinField_management(in_feature, 'Unique_ID', (merge_table_path + "/" + merge_table_name), 'Unique_ID')
-arcpy.Delete_management((merge_table_path + "/" + merge_table_name))
+#Link new results up for Master_List_of_Stations_Results_Tol12000_II_Edits outside_threshold
+out_path = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
+in_fc = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/outside_threshold_update"
+in_table = "outside_threshold.csv"
+replace_attributes(out_path, in_fc, in_table)
 
 #Then for Master_List_of_Stations_Results_Tol12000_II_Edits qc_needs_review
-in_feature = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_needs_review_update"
-merge_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/needs_review.csv"
-merge_table_path = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
-merge_table_name = "qc_needs_review"
-fieldList = arcpy.ListFields(in_feature)
-fields_to_drop = []
-
-for field in fieldList:
-    if field.name not in ['Unique_ID', 'Shape','OBJECTID']:
-        fields_to_drop.append(field.name)
-
-arcpy.DeleteField_management(in_feature, fields_to_drop)
-
-arcpy.TableToTable_conversion(merge_file, merge_table_path, merge_table_name)
-arcpy.JoinField_management(in_feature, 'Unique_ID', (merge_table_path + "/" + merge_table_name), 'Unique_ID')
-arcpy.Delete_management((merge_table_path + "/" + merge_table_name))
+out_path = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb"
+in_fc = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_needs_review_update"
+in_table = "needs_review.csv"
+replace_attributes(out_path, in_fc, in_table)
 
 #Remove duplicates from qc_needs_review_update
 in_file = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList/Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_needs_review_update"
@@ -181,7 +130,7 @@ with arcpy.da.UpdateCursor(in_file, "Duplicate") as cursor:
             cursor.deleteRow()
 
 
-#Select stations that are fully qc'd
+#Select stations that are fully qc'd from the two reviewed fcs
 workspace = "E:/GitHub/ToxicsRedo/StationsToLocate/FinalList"
 arcpy.env.workspace = workspace
 f1 = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_needs_review_update"
@@ -189,39 +138,23 @@ f2 = "Additional_LASAR_Stations_Edits.gdb/All_stations"
 lay1 = "additional_lasar_stations"
 lay2 = "master_list_of_statiosn"
 
-#Define unique values in QAQC2 field of needs_review_update
-valueList = []
-rows = arcpy.SearchCursor(f1)
-for row in rows:
-    valueList.append(row.getValue("QAQC2"))
-
-uniqueSet = set(valueList)
-uniqueList1 = list(uniqueSet)
-uniqueList1.sort()
-del rows
-del row
+#List unique values in the QAQC2 field of needs_review_update and All_stations
+values = [row[0] for row in arcpy.da.SearchCursor(f1, "QAQC2")]
+uniqueValues = set(values)
+uniqueList1 = list(uniqueValues)
 print uniqueList1
 
-valueList = []
-rows = arcpy.SearchCursor(f2)
-for row in rows:
-    valueList.append(row.getValue("QAQC2"))
-
-uniqueSet = set(valueList)
-uniqueList2 = list(uniqueSet)
-uniqueList2.sort()
-del rows
-del row
+values = [row[0] for row in arcpy.da.SearchCursor(f2, "QAQC2")]
+uniqueValues = set(values)
+uniqueList2 = list(uniqueValues)
 print uniqueList2
 
-
-#Remove "remove", "Further review", and "Potential digitization"
+#Remove "remove", "Further review", and "Potential digitization" from this list
 items_to_remove = ["Potential Digitization", "Further Review Needed", "Remove"]
 uniqueList1 = [x for x in uniqueList1 if x  not in items_to_remove]
 uniqueList2 = [x for x in uniqueList2 if x not in items_to_remove]
 
-#Setup Queries
-
+#Setup Queries based on the lists of unique fields with only stations we want to keep
 query1 = """ "QAQC2" in """ + "('" + "', '".join(uniqueList1) +"')"
 query2 = """ "QAQC1" = 'Reviewed' or "QAQC2" in """ + "('" + "', '".join(uniqueList2) +"')"
 
@@ -236,7 +169,7 @@ f3 = "Master_List_of_Stations_Results_Tol12000_II_Edits.gdb/qc_success_update"
 out_file = "All.gdb/All_non_review_stations"
 arcpy.Merge_management([lay1, lay2, f3], out_file)
 
-#Join DEQ stream and lake names
+#Add DEQ stream and lake names using LLID
 in_file = out_file
 stream_names = "F:/Base_Data/DEQ_Data/WQ_2010_IntegratedReport_V3/WQ_2010_IntegratedReport_V3/Assessment.gdb/DEQ_Streams_25APR2013"
 lake_names = "F:/Base_Data/DEQ_Data/WQ_2010_IntegratedReport_V3/WQ_2010_IntegratedReport_V3/Assessment.gdb/DEQLakes_14JUN2013"
@@ -245,7 +178,7 @@ arcpy.JoinField_management(in_file, 'LLID', stream_names, 'LLID', 'NAME')
 arcpy.JoinField_management(in_file, 'LAKE_LLID', lake_names, 'WATERBODYI', 'NAME')
 
 
-#Update field names to meaningful ones.
+#Change these new field names to meaningful ones.
 renameField(in_file, "NAME", "Stream_Name")
 renameField(in_file, "NAME_1", "Lake_Name")
 
